49fde6118f2ce6abb4e0d0bb4a9da405191a2b69
Sergey <sergius2ch@yandex.ru>
2021-01-21 19:30:48 +0500

    ProjectTemplateWithAjax

---
+++ ProjectTemplateWithAjax/ProjectTemplateWithAjax/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectTemplateWithAjax.Repository;
+
+namespace ProjectTemplateWithAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowStudents_V1()
+        {
+            var students = DataManager.GetStudents();
+
+            return View(students);
+        }
+    }
+}
---
+++ ProjectTemplateWithAjax/ProjectTemplateWithAjax/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplateWithAjax.Models
+{
+    public enum AcademicGroup
+    {
+        Pi322, Pi223, Pi124
+    }
+
+    public class Student
+    {
+        public AcademicGroup AcademicGroup { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+    }
+}
