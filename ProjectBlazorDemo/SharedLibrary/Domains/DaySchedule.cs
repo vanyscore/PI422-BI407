0f182b677c7e83d40d5e6dfb186d50b1e88a4c05
Ivan <vanyscore@gmail.com>
2021-04-15 18:40:12 +0500

    added blazor project

---
+++ ProjectBlazorDemo/SharedLibrary/Domains/DaySchedule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Domains
+{
+    public class DaySchedule
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+
+        private static string[] _days =
+        {
+            "вс", "пн", "вт", "ср", "чт", "пт", "сб"
+        };
+
+        public string DayOfWeekRussian
+        {
+            get
+            {
+                return _days[(int)Date.DayOfWeek];
+            }
+        }
+
+        public DateTime Date { get; set; }
+
+        public List<Lesson> Lessons { get; set; }
+
+    }
+}
---
+++ ProjectBlazorDemo/SharedLibrary/Domains/LessonSpan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Domains
+{
+    public class LessonSpan
+    {
+        public TimeSpan Start { get; set; }
+        public TimeSpan Finish { get; set; }
+    }
+}
