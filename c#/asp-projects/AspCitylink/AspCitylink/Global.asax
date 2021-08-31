f0ab137d584c27d0c85dc48218729fe3c431aa14
Ivan <vanyscore@gmail.com>
2021-09-01 03:00:59 +0500

    Удалены папки

--- c#/asp-projects/AspCitylink/AspCitylink/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspCitylink.MvcApplication" Language="C#" %>
--- javascript/Angular/Project_Demo_Angular/Theme10Services/01_Factory_Services/service.js
+++
@@ -1,9 +0,0 @@
-angular
-.module("moduleService", [])
-.factory("logService", function() {
-    return {
-        log: function(text) {
-            console.log(text);
-        }
-    }
-})
